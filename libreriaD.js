5399a0c41ebf4a6fe6836f974437156b12578f32
PCBARBOGIO\Barbogio <barbogioster@gmail.com>
2018-04-02 18:15:06 +0200

    Punti, linee (traslazione)
    Cominciato ad impostare la struttura per animare la traslazione di punti e linee, predisponendo anche la lettura di frame per colore e spessore

--- libreriaD.js
+++ libreriaD.js
@@ -5,172 +5,244 @@
 
   //La libreria come funzione
   function libreriaD(){
-    var _libreria = {};
+    var _libreriaD = {};
 
 		//*************************VARIABILI*******************
 		//tecniche
 		var canvasId="telaID";
 		var canvas;
 		var ctx;
-		var canX,canY;
+		var canvasX,canvasY;
+		var orologio;
+		var timeStamp=0;
+		var deltaTime=20;
+		var uniqueId=0;
+		var codaDisegno=[];
+		
 
 		//opzioni di disegno
-		var coloreSfondo="#FFFFFF";
-		var coloreTratto="#000000";
-		var traccia=true;
-		var coloreRiempimento="#000000";
-		var riempi=false;
-		var spessoreTratto=1;
-		var capiLinee="round"; //bevel,round,miter
-		var giuntiLinee="round"; //butt,round,square
+		var opzioniDisegno={
+			coloreSfondo:"#FFFFFF",
+			coloreTratto:"#000000",
+			traccia:true,
+			coloreRiempimento:"#000000",
+			riempi:false,
+			spessoreTratto:1,
+			capiLinee:"round", //bevel,round,miter
+			giuntiLinee:"round" //butt,round,square
+		}
+		var opzioniDisegnoTemp={
+			coloreSfondo:"#FFFFFF",
+			coloreTratto:"#000000",
+			traccia:true,
+			coloreRiempimento:"#000000",
+			riempi:false,
+			spessoreTratto:1,
+			capiLinee:"round", //butt,round,square
+			giuntiLinee:"round" //bevel,round,miter
+		}
 
 		
 
 
 		//*************************METODI*******************
-		//da chiamare per primissima cosa
-		_libreria.creaTela=function(larghezza, altezza, dove){
+		//da chiamare per primissima cosa, imposta la canvas TODO estenedere a più layer
+		_libreriaD.creaTela=function(larghezza, altezza, dove){
 			var contenitore;
-			canX=larghezza;
-			canY=altezza;
+			canvasX=larghezza;
+			canvasY=altezza;
 			if(dove==null){contenitore=document.querySelector("body");}
 			else{contenitore=document.getElementById(dove);}
-			contenitore.innerHTML="<canvas id="+canvasId+" width=\""+canX+"px\" height=\""+canY+"px\"></canvas>";
+			contenitore.innerHTML="<canvas id="+canvasId+" width=\""+canvasX+"px\" height=\""+canvasY+"px\"></canvas>";
 			canvas=document.getElementById(canvasId);
 			ctx=canvas.getContext("2d");
 			//valori di default
 			this.impostaOpzioniDisegno();
 
-			//ctx.scale(1,-1);
-			//ctx.translate(0,canY);
+			//imposta l'origine degli assi e il verso dell'asse y "standard"
+			ctx.translate(0,canvasY);
+			ctx.scale(1,-1);
 		};
+
 		
 		//OPZIONI DISEGNO
-		//reimposta tutte le opzioni
-		_libreria.impostaOpzioniDisegno=function(){
-			ctx.strokeStyle= coloreTratto;
-			ctx.lineWidth=spessoreTratto;
-			ctx.fillStyle = coloreRiempimento;
-			ctx.lineCap=capiLinee;
-			ctx.lineJoin=giuntiLinee;
+		//reimposta tutte le opzioni, prendendole dalla raccolta di variabili
+		_libreriaD.impostaOpzioniDisegno=function(){
+			ctx.strokeStyle=opzioniDisegno.coloreTratto;
+			ctx.lineWidth=opzioniDisegno.spessoreTratto;
+			ctx.fillStyle=opzioniDisegno.coloreRiempimento;
+			ctx.lineCap=opzioniDisegno.capiLinee;
+			ctx.lineJoin=opzioniDisegno.giuntiLinee;
 		}
-		//stili
-		_libreria.tratto=function(colore){
+		//slva le opzioni di disegno in una variabile temporanea
+		_libreriaD.salvaOpzioniDisegno=function(){
+			opzioniDisegnoTemp.coloreTratto=opzioniDisegno.coloreTratto;
+			opzioniDisegnoTemp.spessoreTratto=opzioniDisegno.spessoreTratto;
+			opzioniDisegnoTemp.coloreRiempimento=opzioniDisegno.coloreRiempimento;
+			opzioniDisegnoTemp.capiLinee=opzioniDisegno.capiLinee;
+			opzioniDisegnoTemp.giuntiLinee=opzioniDisegno.giuntiLinee;
+		}
+		//carica le opzioni dalla variabile temporanea
+		_libreriaD.caricaOpzioniDisegno=function(){
+			opzioniDisegno.coloreTratto=opzioniDisegnoTemp.coloreTratto;
+			opzioniDisegno.spessoreTratto=opzioniDisegnoTemp.spessoreTratto;
+			opzioniDisegno.coloreRiempimento=opzioniDisegnoTemp.coloreRiempimento;
+			opzioniDisegno.capiLinee=opzioniDisegnoTemp.capiLinee;
+			opzioniDisegno.giuntiLinee=opzioniDisegnoTemp.giuntiLinee;
+		}
+		//imposta i vari stili, modificando la raccolta degli stili attuali
+		_libreriaD.tratto=function(colore){
 			if(colore!=null){
-				coloreTratto=colore;
+				opzioniDisegno.coloreTratto=colore;
 				this.impostaOpzioniDisegno();
 				traccia=true;
 			}else{
 				traccia=false;
 			}
 		}
-		_libreria.riempimento=function(colore){
+		_libreriaD.spessore=function(spessore){
+			if(spessore>0){
+				opzioniDisegno.spessoreTratto=spessore;
+				this.impostaOpzioniDisegno();
+				traccia=true;
+			}else{
+				traccia=false;
+			}
+		}
+		_libreriaD.riempimento=function(colore){
 			if(colore!=null){
-				coloreRiempimento=colore;
+				opzioniDisegno.coloreRiempimento=colore;
 				this.impostaOpzioniDisegno();
 				riempi=true;
 			}else{riempi=false;}
 		}
-		_libreria.spessore=function(spessore){
-			if(spessore>0){
-				spessoreTratto=spessore;
+		_libreriaD.capi=function(capi){
+			if(capi=="round"||capi=="butt"||capi=="square"){
+				opzioniDisegno.capiLinee=capi;
 				this.impostaOpzioniDisegno();
-				traccia=true;
-			}else{
-				traccia=false;
+				riempi=true;
 			}
 		}
-		_libreria.sfondo=function(colore){
+		_libreriaD.giunti=function(giunti){
+			if(capi=="round"||capi=="bevel"||capi=="miter"){
+				opzioniDisegno.giuntiLinee=giunti;
+				this.impostaOpzioniDisegno();
+				riempi=true;
+			}
+		}
+		_libreriaD.sfondo=function(colore){
 			canvas.style.backgroundColor=colore;
 		}
-		_libreria.cancella=function(){
-			ctx.clearRect(0,0,canX,canY);
+		_libreriaD.cancella=function(){
+			ctx.clearRect(0,0,canvasX,canvasY);
+		}
+
+		//avvia il ciclo di animazione passando il contesto, per chiamare le funzioni pubbliche
+		_libreriaD.anima=function(){
+			contesto=this;
+			orologio=setInterval(function(){ciclo(contesto);},deltaTime);
+		}
+		//ciclo di animazione vero e proprio
+		var ciclo=function(contesto){
+			contesto.cancella();
+			codaDisegno.forEach(function(item){
+				disegna(item);
+			});
+			timeStamp++;
+		}
+		//smista le chiamate al disegno dei vari oggetti
+		var disegna=function(oggetto){
+			switch(oggetto.tipo){
+				case "punto":
+				disegnaPunto(oggetto);
+				break;
+				case "linea":
+				disegnaLinea(oggetto);
+				break;
+			}
 		}
 
 		//figure base
-		_libreria.punto=function(X,Y){
-			var fillTemp=coloreRiempimento;
-			var riempiTemp=riempi;
-			var tracciaTemp=traccia;
-			riempi=true;
-			traccia=false;
-			spessore=spessoreTratto;
-			if(spessore<0.5) spessore=0.5;
-			this.riempimento(coloreTratto);
-			this.cerchio(X,Y,spessore/2);
-			this.riempimento(fillTemp);
-			riempi=riempiTemp;
-			traccia=tracciaTemp;
-		}
-		_libreria.linea=function(Xa,Ya,Xb,Yb){
-			var tracciaTemp=traccia;
-			ctx.beginPath();
-			ctx.moveTo(Xa,Ya);
-			ctx.lineTo(Xb,Yb);
-			ctx.stroke();
-			traccia=tracciaTemp;
-		}
-		_libreria.semiretta=function(Xa,Ya,Xb,Yb){
-			var m=(Yb-Ya)/(Xb-Xa);
-			var Xbordo=0,Ybordo=0;
-			if(canX>canY){//intersezione con bordo verticale
-				Xbordo=0;
-				if(Xb>Xa) Xbordo=canX;
-				Ybordo=m*(Xbordo-Xa)+Ya;
-			}else{
-				//intersezione con bordo orizzontale
-				Ybordo=0;
-				if(Yb>Ya) Ybordo=canY;
-				Xbordo=(Ybordo-Ya+m*Xa)/m;
+		_libreriaD.punto=function(Xa,Ya){
+			var identita=uniqueId;
+			uniqueId++;
+			var oggetto={
+				id:identita,
+				tipo:"punto",
+				colore:[opzioniDisegno.coloreTratto],
+				spessore:[opzioniDisegno.spessoreTratto],
+				angolo:[0],
+				posizione:[[Xa,Ya]]
 			}
-			this.linea(Xa,Ya,Xbordo,Ybordo);
-		}
-		_libreria.retta=function(Xa,Ya,Xb,Yb){
-			var m=(Yb-Ya)/(Xb-Xa);
-			var X0,X1,Y0,Y1;
-			if(canX>canY){//intersezione con bordo verticale
-				X0=0; X1=canX;
-				Y0=m*(X0-Xa)+Ya;
-				Y1=m*(X1-Xa)+Ya;
-			}else{
-				//intersezione con bordo orizzontale
-				Y0=0;Y1=canY;
-				X0=(Y0-Ya+m*Xa)/m;
-				X1=(Y1-Ya+m*Xa)/m;
-			}
-			this.linea(X0,Y0,X1,Y1);
-		}
-		_libreria.spezzata=function(args){
-			if(arguments.length>0&&arguments.length%2!=0){console.log("Numero di argomenti sbagliato!");}
-			else{
-				ctx.beginPath();
-				ctx.moveTo(arguments[0],arguments[1]);
-				for(var i=2;i<arguments.length;i=i+2){
-					ctx.lineTo(arguments[i],arguments[i+1]);
+			codaDisegno.push(oggetto);
+
+			this.trasla=function(X,Y,tempo){
+				var indice=oggetto.posizione.length;
+				var valore=oggetto.posizione[indice-1];
+				var passi=tempo/deltaTime;
+				var passoX=X/passi;
+				var passoY=Y/passi;
+				for(var i=0;i<passi-1;i++){
+					oggetto.posizione.push([valore[0]+i*passoX,valore[1]+i*passoY]);
 				}
-				if(riempi) ctx.fill();
-				if(traccia) ctx.stroke();
+				oggetto.posizione.push([valore[0]+X,valore[1]+Y]);
 			}
-		}
-		_libreria.arco=function(Xa,Ya,Xb,Yb,Xc,Yc){//da fare
-			
-		}
-		_libreria.cerchio=function(Xc,Yc,raggio){
-			ctx.beginPath();
-			ctx.arc(Xc,Yc,raggio,0,2*Math.PI);
-			if(riempi) ctx.fill();
-			if(traccia) ctx.stroke();
 		}
 
-    return _libreria;
+		var disegnaPunto=function(oggetto){
+			var posizione=oggetto.posizione[Math.min(timeStamp,oggetto.posizione.length-1)];
+			var colore=oggetto.colore[Math.min(timeStamp,oggetto.colore.length-1)];
+			var spessore=oggetto.spessore[Math.min(timeStamp,oggetto.spessore.length-1)];
+			ctx.fillStyle=colore;
+			ctx.beginPath();
+			ctx.arc(posizione[0],posizione[1],spessore,0,2*Math.PI);
+			ctx.fill();
+		}
+
+		_libreriaD.linea=function(Xa,Ya,Xb,Yb){
+			var identita=uniqueId;
+			uniqueId++;
+			var oggetto={
+				id:identita,
+				tipo:"linea",
+				colore:[opzioniDisegno.coloreTratto],
+				spessore:[opzioniDisegno.spessoreTratto],
+				angolo:[0],
+				posizione:[[Xa,Ya,Xb,Yb]]
+			}
+			codaDisegno.push(oggetto);
+
+			this.trasla=function(X,Y,tempo){
+				var indice=oggetto.posizione.length;
+				var valore=oggetto.posizione[indice-1];
+				var passi=tempo/deltaTime;
+				var passoX=X/passi;
+				var passoY=Y/passi;
+				for(var i=0;i<passi-1;i++){
+					oggetto.posizione.push([valore[0]+i*passoX,valore[1]+i*passoY,valore[2]+i*passoX,valore[3]+i*passoY]);
+				}
+				oggetto.posizione.push([valore[0]+X,valore[1]+Y,valore[2]+X,valore[3]+Y]);
+			}
+		}
+
+		var disegnaLinea=function(oggetto){
+			var posizione=oggetto.posizione[Math.min(timeStamp,oggetto.posizione.length-1)];
+			var colore=oggetto.colore[Math.min(timeStamp,oggetto.colore.length-1)];
+			var spessore=oggetto.spessore[Math.min(timeStamp,oggetto.spessore.length-1)];
+			ctx.strokeStyle=colore;
+			ctx.lineWidth=spessore;
+			ctx.beginPath();
+			ctx.moveTo(posizione[0],posizione[1]);
+			ctx.lineTo(posizione[2],posizione[3]);
+			ctx.stroke();
+		}
+
+
+    return _libreriaD;
   }
 
   //Salvo la variabile in window per renderla accessibile
   if(typeof(window.D) === 'undefined'){
     window.D = libreriaD();
   }
-})(window); // We send the window variable withing our function
-
-
-// Then we can call it using
-//console.log(D);
+})(window); //eseguo la funzione su window al caricamento della libreria
